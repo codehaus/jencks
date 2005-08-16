116677f7bb2524aa95ea7cc26dc5fb252e52cdd9
jstrachan <jstrachan@4853a6bc-9e15-0410-a47b-cdba9ba9a9a3>
2005-08-16 14:35:43 +0000

    Initial revision

---
+++ trunk/jencks/README.txt
@@ -0,0 +1,2 @@
+Jencks is a POJO and Spring extension to the Geronimo JCA container
+to support Message Driven POJOs.
---
+++ trunk/jencks/src/main/java/org/jencks/factory/BootstrapContextFactoryBean.java
@@ -0,0 +1,159 @@
+/**
+ * 
+ * Copyright 2005 LogicBlaze, Inc. http://www.logicblaze.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ * 
+ **/
+package org.jencks.factory;
+
+import org.apache.geronimo.connector.BootstrapContextImpl;
+import org.apache.geronimo.connector.work.GeronimoWorkManager;
+import org.apache.geronimo.gbean.ReferenceCollection;
+import org.apache.geronimo.transaction.ExtendedTransactionManager;
+import org.apache.geronimo.transaction.context.TransactionContextManager;
+import org.apache.geronimo.transaction.log.UnrecoverableLog;
+import org.apache.geronimo.transaction.manager.TransactionLog;
+import org.apache.geronimo.transaction.manager.TransactionManagerImpl;
+import org.apache.geronimo.transaction.manager.XidImporter;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.resource.spi.BootstrapContext;
+import javax.resource.spi.work.WorkManager;
+import javax.transaction.xa.XAException;
+
+/**
+ * A Spring {@link FactoryBean} for creating a {@link BootstrapContext} for the JCA container
+ * with the {@link WorkManager} and {@link ExtendedTransactionManager}.
+ *
+ * @version $Revision$
+ */
+public class BootstrapContextFactoryBean implements FactoryBean, InitializingBean {
+
+    private BootstrapContext bootstrapContext;
+    private GeronimoWorkManager workManager;
+    private TransactionContextManager transactionContextManager;
+    private int threadPoolSize = 30;
+    private ExtendedTransactionManager transactionManager;
+    private XidImporter xidImporter;
+    private int defaultTransactionTimeoutSeconds = 600;
+    private TransactionLog transactionLog;
+    private ReferenceCollection resourceManagers;
+
+    public Object getObject() throws Exception {
+        return bootstrapContext;
+    }
+
+    public Class getObjectType() {
+        return BootstrapContext.class;
+    }
+
+    public boolean isSingleton() {
+        return true;
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        bootstrapContext = new BootstrapContextImpl(getWorkManager());
+        getWorkManager().doStart();
+    }
+
+    public GeronimoWorkManager getWorkManager() throws XAException {
+        if (workManager == null) {
+            workManager = createWorkManager();
+        }
+        return workManager;
+    }
+
+    public void setWorkManager(GeronimoWorkManager workManager) {
+        this.workManager = workManager;
+    }
+
+    public TransactionContextManager getTransactionContextManager() throws XAException {
+        if (transactionContextManager == null) {
+            transactionContextManager = createTransactionContextManager();
+        }
+        return transactionContextManager;
+    }
+
+    public void setTransactionContextManager(TransactionContextManager transactionContextManager) {
+        this.transactionContextManager = transactionContextManager;
+    }
+
+    public int getThreadPoolSize() {
+        return threadPoolSize;
+    }
+
+    public void setThreadPoolSize(int threadPoolSize) {
+        this.threadPoolSize = threadPoolSize;
+    }
+
+    public ExtendedTransactionManager getTransactionManager() throws XAException {
+        if (transactionManager == null) {
+            transactionManager = new TransactionManagerImpl(getDefaultTransactionTimeoutSeconds(), getTransactionLog(), getResourceManagers());
+        }
+        return transactionManager;
+    }
+
+    public void setTransactionManager(ExtendedTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    public XidImporter getXidImporter() {
+        if (xidImporter == null && transactionManager instanceof XidImporter) {
+            xidImporter = (XidImporter) transactionManager;
+        }
+        return xidImporter;
+    }
+
+    public void setXidImporter(XidImporter xidImporter) {
+        this.xidImporter = xidImporter;
+    }
+
+    public int getDefaultTransactionTimeoutSeconds() {
+        return defaultTransactionTimeoutSeconds;
+    }
+
+    public void setDefaultTransactionTimeoutSeconds(int defaultTransactionTimeoutSeconds) {
+        this.defaultTransactionTimeoutSeconds = defaultTransactionTimeoutSeconds;
+    }
+
+    public TransactionLog getTransactionLog() {
+        if (transactionLog == null) {
+            transactionLog = new UnrecoverableLog();
+        }
+        return transactionLog;
+    }
+
+    public void setTransactionLog(TransactionLog transactionLog) {
+        this.transactionLog = transactionLog;
+    }
+
+    public ReferenceCollection getResourceManagers() {
+        return resourceManagers;
+    }
+
+    public void setResourceManagers(ReferenceCollection resourceManagers) {
+        this.resourceManagers = resourceManagers;
+    }
+
+    // Implementation methods
+    //-------------------------------------------------------------------------
+    protected TransactionContextManager createTransactionContextManager() throws XAException {
+        return new TransactionContextManager(getTransactionManager(), getXidImporter());
+    }
+
+    protected GeronimoWorkManager createWorkManager() throws XAException {
+        return new GeronimoWorkManager(getThreadPoolSize(), getTransactionContextManager());
+    }
+}
