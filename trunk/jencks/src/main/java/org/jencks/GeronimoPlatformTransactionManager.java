0906811e0c00f0d0f8bd4da15ec2395d00814028
gnt <gnt@4853a6bc-9e15-0410-a47b-cdba9ba9a9a3>
2006-12-15 14:00:31 +0000

    JCA-34: The jencks PlatformTransactionManager implementation does not synchronize resources with spring

--- trunk/jencks/src/main/java/org/jencks/GeronimoPlatformTransactionManager.java
+++ trunk/jencks/src/main/java/org/jencks/GeronimoPlatformTransactionManager.java
@@ -16,35 +16,52 @@
 package org.jencks;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.transaction.Status;
+import javax.transaction.SystemException;
+import javax.transaction.Transaction;
 import javax.transaction.xa.XAException;
 
 import org.apache.geronimo.transaction.manager.GeronimoTransactionManager;
 import org.apache.geronimo.transaction.manager.TransactionLog;
+import org.apache.geronimo.transaction.manager.TransactionManagerMonitor;
 import org.apache.geronimo.transaction.manager.XidFactory;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionException;
 import org.springframework.transaction.jta.JtaTransactionManager;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import edu.emory.mathcs.backport.java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @version $Revision$ $Date$
  */
 public class GeronimoPlatformTransactionManager extends GeronimoTransactionManager implements PlatformTransactionManager {
+    
     private final PlatformTransactionManager platformTransactionManager;
+    private final Map suspendedResources = new ConcurrentHashMap();
 
     public GeronimoPlatformTransactionManager() throws XAException {
         platformTransactionManager = new JtaTransactionManager(this, this);
+        registerTransactionAssociationListener();
     }
 
     public GeronimoPlatformTransactionManager(int defaultTransactionTimeoutSeconds) throws XAException {
         super(defaultTransactionTimeoutSeconds);
         platformTransactionManager = new JtaTransactionManager(this, this);
+        registerTransactionAssociationListener();
     }
 
     public GeronimoPlatformTransactionManager(int defaultTransactionTimeoutSeconds, TransactionLog transactionLog) throws XAException {
         super(defaultTransactionTimeoutSeconds, transactionLog);
         platformTransactionManager = new JtaTransactionManager(this, this);
+        registerTransactionAssociationListener();
     }
 
     public GeronimoPlatformTransactionManager(int defaultTransactionTimeoutSeconds,
@@ -54,6 +71,7 @@ public class GeronimoPlatformTransactionManager extends GeronimoTransactionManag
 
         super(defaultTransactionTimeoutSeconds, xidFactory, transactionLog, resourceManagers);
         platformTransactionManager = new JtaTransactionManager(this, this);
+        registerTransactionAssociationListener();
     }
 
     public TransactionStatus getTransaction(TransactionDefinition definition) throws TransactionException {
@@ -67,4 +85,92 @@ public class GeronimoPlatformTransactionManager extends GeronimoTransactionManag
     public void rollback(TransactionStatus status) throws TransactionException {
         platformTransactionManager.rollback(status);
     }
+
+    protected void registerTransactionAssociationListener() {
+        addTransactionAssociationListener(new TransactionManagerMonitor() {
+            public void threadAssociated(Transaction transaction) {
+                try {
+                    if (transaction.getStatus() == Status.STATUS_ACTIVE) {
+                        SuspendedResourcesHolder holder = (SuspendedResourcesHolder) suspendedResources.remove(transaction);
+                        if (holder != null && holder.getSuspendedSynchronizations() != null) {
+                            TransactionSynchronizationManager.setActualTransactionActive(true);
+                            TransactionSynchronizationManager.setCurrentTransactionReadOnly(holder.isReadOnly());
+                            TransactionSynchronizationManager.setCurrentTransactionName(holder.getName());
+                            TransactionSynchronizationManager.initSynchronization();
+                            for (Iterator it = holder.getSuspendedSynchronizations().iterator(); it.hasNext();) {
+                                TransactionSynchronization synchronization = (TransactionSynchronization) it.next();
+                                synchronization.resume();
+                                TransactionSynchronizationManager.registerSynchronization(synchronization);
+                            }
+                        }
+                    }
+                } catch (SystemException e) {
+                    return;
+                }
+            }
+            public void threadUnassociated(Transaction transaction) {
+                try {
+                    if (transaction.getStatus() == Status.STATUS_ACTIVE) {
+                        if (TransactionSynchronizationManager.isSynchronizationActive()) {
+                            List suspendedSynchronizations = TransactionSynchronizationManager.getSynchronizations();
+                            for (Iterator it = suspendedSynchronizations.iterator(); it.hasNext();) {
+                                ((TransactionSynchronization) it.next()).suspend();
+                            }
+                            TransactionSynchronizationManager.clearSynchronization();
+                            String name = TransactionSynchronizationManager.getCurrentTransactionName();
+                            TransactionSynchronizationManager.setCurrentTransactionName(null);
+                            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+                            TransactionSynchronizationManager.setCurrentTransactionReadOnly(false);
+                            TransactionSynchronizationManager.setActualTransactionActive(false);
+                            SuspendedResourcesHolder holder = new SuspendedResourcesHolder(null, suspendedSynchronizations, name, readOnly);
+                            suspendedResources.put(transaction, holder);
+                        }
+                    }
+                } catch (SystemException e) {
+                    return;
+                }
+            }
+        });
+    }
+
+    /**
+     * Holder for suspended resources.
+     * Used internally by <code>suspend</code> and <code>resume</code>.
+     */
+    private static class SuspendedResourcesHolder {
+
+        private final Object suspendedResources;
+
+        private final List suspendedSynchronizations;
+
+        private final String name;
+
+        private final boolean readOnly;
+
+        public SuspendedResourcesHolder(
+                Object suspendedResources, List suspendedSynchronizations, String name, boolean readOnly) {
+
+            this.suspendedResources = suspendedResources;
+            this.suspendedSynchronizations = suspendedSynchronizations;
+            this.name = name;
+            this.readOnly = readOnly;
+        }
+
+        public Object getSuspendedResources() {
+            return suspendedResources;
+        }
+
+        public List getSuspendedSynchronizations() {
+            return suspendedSynchronizations;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public boolean isReadOnly() {
+            return readOnly;
+        }
+    }
+
 }
