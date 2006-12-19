c50061205f43a13c5f142c49146782d8ad6a35f9
gnt <gnt@4853a6bc-9e15-0410-a47b-cdba9ba9a9a3>
2006-12-19 10:32:36 +0000

    JCA-35: Dedicated ActiveMQ pooled ConnectionFactory with XA / JCA support

--- trunk/jencks-amqpool/src/test/java/org/jencks/amqpool/PoolLoadTest.java
+++ trunk/jencks-amqpool/src/test/java/org/jencks/amqpool/PoolLoadTest.java
@@ -1,4 +1,4 @@
-package org.jencks;
+package org.jencks.amqpool;
 
 import java.util.concurrent.CountDownLatch;
 
@@ -6,13 +6,19 @@ import javax.jms.ConnectionFactory;
 
 import junit.framework.TestCase;
 
-import org.apache.activemq.pool.PooledConnectionFactory;
+import org.jencks.GeronimoPlatformTransactionManager;
 import org.springframework.jms.core.JmsTemplate;
 
 public class PoolLoadTest extends TestCase {
 
     public final static String TEST_MESSAGE = "test message";
     public final static String INPUT_QUEUE = "test.in";
+    
+    private GeronimoPlatformTransactionManager txManager;
+    
+    public PoolLoadTest() throws Exception {
+        txManager = new GeronimoPlatformTransactionManager();
+    }
     
     public void testSyncSend() throws Exception {
         loadTest("tcp://localhost:61616?jms.useAsyncSend=false");
@@ -23,7 +29,8 @@ public class PoolLoadTest extends TestCase {
     }
     
     protected void loadTest(String url) throws Exception {
-        PooledConnectionFactory factory = new PooledConnectionFactory(url);
+        JcaPooledConnectionFactory factory = new JcaPooledConnectionFactory(url);
+        factory.setTransactionManager(txManager);
 
         System.err.println("==============");
         System.err.println("Using url: " + url);
@@ -33,17 +40,26 @@ public class PoolLoadTest extends TestCase {
             // Warm up
             System.err.println("======== " + max + " connections ======");
             System.err.println("Warm up");
-            runThreads(1, max, factory);
+            runThreads(1, max, factory, 0);
             System.err.println("================");
             
-            runThreads(1, 1000, factory);
-            runThreads(10, 100, factory);
-            runThreads(100, 10, factory);
-            runThreads(1000, 1, factory);
+            //runThreads(1, 1000, factory, 0);
+            //runThreads(1, 1000, factory, 1);
+            //runThreads(1, 1000, factory, 10);
+            //runThreads(1, 1000, factory, 1000);
+            runThreads(10, 100, factory, 0);
+            runThreads(10, 100, factory, 1);
+            runThreads(10, 100, factory, 10);
+            runThreads(10, 100, factory, 100);
+            runThreads(100, 10, factory, 0);
+            runThreads(100, 10, factory, 1);
+            runThreads(100, 10, factory, 10);
+            runThreads(1000, 1, factory, 0);
+            runThreads(1000, 1, factory, 1);
         }
     }
     
-    protected long runThreads(final int nbThreads, final int msgsPerThread, final ConnectionFactory factory) throws InterruptedException {
+    protected long runThreads(final int nbThreads, final int msgsPerThread, final ConnectionFactory factory, final int txBatch) throws InterruptedException {
         final JmsTemplate template = new JmsTemplate(factory);
         final CountDownLatch sem = new CountDownLatch(1);
         final CountDownLatch l = new CountDownLatch(nbThreads * msgsPerThread);
@@ -57,10 +73,27 @@ public class PoolLoadTest extends TestCase {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
-                    for (int msg = 0; msg < msgsPerThread; msg++) {
-                        //System.out.println("Sending message " + msg + " from thread " + Thread.currentThread().getName());
-                        template.convertAndSend(INPUT_QUEUE, TEST_MESSAGE);
-                        l.countDown();
+                    try {
+                        int i = 0;
+                        for (int msg = 0; msg < msgsPerThread; msg++) {
+                            //System.out.println("Sending message " + msg + " from thread " + Thread.currentThread().getName());
+                            if (txBatch > 0) {
+                                if (i++ == 0) {
+                                    txManager.begin();
+                                }
+                            }
+                            template.convertAndSend(INPUT_QUEUE, TEST_MESSAGE);
+                            if (txBatch > 0) {
+                                if (i == txBatch || msg == msgsPerThread - 1) {
+                                    txManager.commit();
+                                    i = 0;
+                                }
+                            }
+                            l.countDown();
+                        }
+                    } catch (Exception e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
                     }
                 }
             };
@@ -75,6 +108,7 @@ public class PoolLoadTest extends TestCase {
         for (int i = 0; i < nbThreads; i++) {
             threads[i].join();
         }
+        System.err.println("TxBatch: " + txBatch);
         System.err.println("Threads: " + nbThreads + ", Msg/Thread: " + msgsPerThread);
         System.err.println("Sent " + (nbThreads * msgsPerThread) + " messages in " + ((t1 - t0) / 1000.0) + " secs");
         System.err.println("Throughput: " + ((nbThreads * msgsPerThread * 1000.0) / (t1 - t0)) + " msgs/sec");
